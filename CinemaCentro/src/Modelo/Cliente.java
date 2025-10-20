40d987fc5adfd7aa0515d0f4e1c0e10a2c214168
Naiara B. <naiaraayelen@gmail.com>
2025-10-20 18:49:31 -0300

    cliente listo

--- CinemaCentro/src/Modelo/Cliente.java
+++ CinemaCentro/src/Modelo/Cliente.java
@@ -6,6 +6,7 @@
 package Modelo;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 /**
  *
@@ -38,6 +39,70 @@ public class Cliente {
         this.nombre = nombre;
         this.apellido = apellido;
     }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(LocalDate fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    @Override
+    public boolean equals(Object a) {
+        if(a == this){
+            return true;
+        }
+        if(a.getClass() != this.getClass() || this.dni == 0){
+            return false;
+        }
+        Cliente cliente = (Cliente) a;
+        return cliente.dni == this.dni;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id_cliente);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente: \n" + "\tid_cliente: " + id_cliente + "\n \tDNI: " + dni  + "\n \tNombre: " + nombre + "\n \tApellido: " + apellido + "\n \tFecha de nacimiento: " + fecha_nac;
+    }
+
+    
     
     
 }
