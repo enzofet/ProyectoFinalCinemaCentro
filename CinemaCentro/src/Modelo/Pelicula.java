a9cfb378a6deb9bd970ecb48a8ed00b73464fdff
josebossa3-cmyk <jose.bossa.3@gmail.com>
2025-10-20 19:32:34 -0300

    Clase Pelicula

--- CinemaCentro/src/Modelo/Pelicula.java
+++ CinemaCentro/src/Modelo/Pelicula.java
@@ -5,10 +5,181 @@
  */
 package Modelo;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *
  * @author Naiara
  */
 public class Pelicula {
+    private int id_Pelicula;
+    private String Titulo;
+    private String Director;
+    private String Reparto;
+    private String Pais_Origen;
+    private String Genero;
+    private int enCartelera;
+    private Date Estreno;
+    private int Estado;
+
+    public Pelicula() {
+        this.id_Pelicula=-1;
+    }
+
+    public Pelicula(int id_Pelicula, String Titulo, String Director, String Reparto, String Pais_Origen, String Genero, int enCartelera, Date Estreno, int Estado) {
+        this.id_Pelicula = id_Pelicula;
+        this.Titulo = Titulo;
+        this.Director = Director;
+        this.Reparto = Reparto;
+        this.Pais_Origen = Pais_Origen;
+        this.Genero = Genero;
+        this.enCartelera = enCartelera;
+        this.Estreno = Estreno;
+        this.Estado = Estado;
+    }
+
+    public Pelicula(String Titulo, String Director, String Reparto, String Pais_Origen, String Genero, int enCartelera, Date Estreno, int Estado) {
+        this.Titulo = Titulo;
+        this.Director = Director;
+        this.Reparto = Reparto;
+        this.Pais_Origen = Pais_Origen;
+        this.Genero = Genero;
+        this.enCartelera = enCartelera;
+        this.Estreno = Estreno;
+        this.Estado = Estado;
+    }
+
+    public int getId_Pelicula() {
+        return id_Pelicula;
+    }
+
+    public void setId_Pelicula(int id_Pelicula) {
+        this.id_Pelicula = id_Pelicula;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String Director) {
+        this.Director = Director;
+    }
+
+    public String getReparto() {
+        return Reparto;
+    }
+
+    public void setReparto(String Reparto) {
+        this.Reparto = Reparto;
+    }
+
+    public String getPais_Origen() {
+        return Pais_Origen;
+    }
+
+    public void setPais_Origen(String Pais_Origen) {
+        this.Pais_Origen = Pais_Origen;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public int getEnCartelera() {
+        return enCartelera;
+    }
+
+    public void setEnCartelera(int enCartelera) {
+        this.enCartelera = enCartelera;
+    }
+
+    public Date getEstreno() {
+        return Estreno;
+    }
+
+    public void setEstreno(Date Estreno) {
+        this.Estreno = Estreno;
+    }
+
+    public int getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(int Estado) {
+        this.Estado = Estado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.id_Pelicula;
+        hash = 97 * hash + Objects.hashCode(this.Titulo);
+        hash = 97 * hash + Objects.hashCode(this.Director);
+        hash = 97 * hash + Objects.hashCode(this.Reparto);
+        hash = 97 * hash + Objects.hashCode(this.Pais_Origen);
+        hash = 97 * hash + Objects.hashCode(this.Genero);
+        hash = 97 * hash + this.enCartelera;
+        hash = 97 * hash + Objects.hashCode(this.Estreno);
+        hash = 97 * hash + this.Estado;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pelicula other = (Pelicula) obj;
+        if (this.id_Pelicula != other.id_Pelicula) {
+            return false;
+        }
+        if (this.enCartelera != other.enCartelera) {
+            return false;
+        }
+        if (this.Estado != other.Estado) {
+            return false;
+        }
+        if (!Objects.equals(this.Titulo, other.Titulo)) {
+            return false;
+        }
+        if (!Objects.equals(this.Director, other.Director)) {
+            return false;
+        }
+        if (!Objects.equals(this.Reparto, other.Reparto)) {
+            return false;
+        }
+        if (!Objects.equals(this.Pais_Origen, other.Pais_Origen)) {
+            return false;
+        }
+        if (!Objects.equals(this.Genero, other.Genero)) {
+            return false;
+        }
+        if (!Objects.equals(this.Estreno, other.Estreno)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     
 }
