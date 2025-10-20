63c620f7045d9fddc8c65cbe4c853c5eac9ec176
Gonzalo Achucarro <achucarrogonza8@gmail.com>
2025-10-20 19:59:48 -0300

    ConexionBD actualizada

--- CinemaCentro/src/Controlador/ConexionBD.java
+++ CinemaCentro/src/Controlador/ConexionBD.java
@@ -5,12 +5,38 @@
  */
 package Controlador;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Enzo_2
  */
 public class ConexionBD {
+    private static Connection conn = null;
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/cinemacentro";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection(){
+        try {
+            conn = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
     
-     
-    
+    public static void closeConnection(){
+        try{
+            if(conn != null){
+                conn.close();
+                conn = null;
+            } 
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
