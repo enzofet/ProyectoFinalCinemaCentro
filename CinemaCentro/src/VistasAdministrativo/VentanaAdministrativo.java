947c95b26321fd78521c2e852024430067d7432b
Enzo Fet <enzofet1998@gmail.com>
2025-10-20 19:00:58 -0300

    Diviendo las vistas

--- CinemaCentro/src/VistasAdministrativo/VentanaAdministrativo.java
+++ CinemaCentro/src/VistasAdministrativo/VentanaAdministrativo.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Vistas;
+package VistasAdministrativo;
 
 /**
  *
  * @author Enzo_2
  */
-public class ventanaprincipal extends javax.swing.JFrame {
+public class VentanaAdministrativo extends javax.swing.JFrame {
 
     /**
-     * Creates new form ventanaprincipal
+     * Creates new form VentanaAdministrativo
      */
-    public ventanaprincipal() {
+    public VentanaAdministrativo() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class ventanaprincipal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ventanaprincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaAdministrativo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ventanaprincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaAdministrativo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ventanaprincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaAdministrativo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ventanaprincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaAdministrativo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ventanaprincipal().setVisible(true);
+                new VentanaAdministrativo().setVisible(true);
             }
         });
     }
