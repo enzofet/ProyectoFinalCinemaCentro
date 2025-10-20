acd79a30e368dbbf06e8735067e8694e68337177
Gonzalo Achucarro <achucarrogonza8@gmail.com>
2025-10-20 19:42:08 -0300

    Revert "ConexionBD actualizada"
    This reverts commit 792e5e5b863c1ba2feaab8a2943c00a861ccc2b9.

--- CinemaCentro/src/Controlador/ConexionBD.java
+++ CinemaCentro/src/Controlador/ConexionBD.java
@@ -5,46 +5,12 @@
  */
 package Controlador;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 /**
  *
  * @author Enzo_2
  */
 public class ConexionBD {
     
-    private static Connection conn = null;
-    private static final String DRIVER = "org.mariadb.jdbc.Driver";
-    private static final String URL = "jdbc:mariadb://localhost/cinemacentro";
-    private static final String USUARIO = "root";
-    private static final String PASSWORD = "";
-    
-    public static Connection getConnection(){
-        try {
-            if(conn == null){
-            Class.forName(DRIVER);
-            conn = DriverManager.getConnection(URL, USUARIO, PASSWORD);
-            }     
-        } catch (ClassNotFoundException e){
-            e.printStackTrace();
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-        return conn;
-    }
-    
-    public static void closeConnection(){
-        try{
-            if(conn != null){
-                conn.close();
-                conn = null;
-            } 
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
      
     
 }
