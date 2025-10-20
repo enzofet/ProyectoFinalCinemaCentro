5377c8f471c3f3ee899f4d6ee7b86b01813eaae9
Naiara B. <naiaraayelen@gmail.com>
2025-10-20 18:36:24 -0300

    clases modelo

--- CinemaCentro/src/Modelo/Cliente.java
+++ CinemaCentro/src/Modelo/Cliente.java
@@ -5,10 +5,39 @@
  */
 package Modelo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Enzo_2
  */
 public class Cliente {
+    
+    private int id_cliente;
+    private int dni;
+    private LocalDate fecha_nac;
+    private String nombre;
+    private String apellido;
+    
+    public Cliente(){
+        this.id_cliente=-1;
+    }
+    
+    public Cliente(int dni, LocalDate fecha_nac, String nombre, String apellido){
+        this.id_cliente = -1;
+        this.dni = dni;
+        this.fecha_nac = fecha_nac;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    public Cliente(int id_cliente, int dni, LocalDate fecha_nac, String nombre, String apellido){
+        this.id_cliente = id_cliente;
+        this.dni = dni;
+        this.fecha_nac = fecha_nac;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
     
 }
